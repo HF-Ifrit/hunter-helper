b3229bcd5fcf2fa6d7b3114467698660592c3f73
Kaius Reed <kaikai11@live.com>
2024-11-03 21:13:08 -0600

    Filter weapons by weapon type

--- app/_components/WeaponElementFilter.tsx
+++ app/_components/WeaponElementFilter.tsx
@@ -1,6 +1,7 @@
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import { useState } from "react";
 import useSWR from "swr";
 import { fetcher } from "../helpers";
 import WeaponCard from "./WeaponCard";
@@ -21,11 +22,15 @@ const TYPES = [
   "hammer",
   "switch-axe",
 ];
+
 export default function WeaponElementFilter({
   element,
 }: {
   element: ElementType;
 }) {
+  const [selectedWeaponTypes, setSelectedWeaponTypes] = useState<
+    Map<string, boolean>
+  >(new Map(TYPES.map((type) => [type, true])));
   const {
     data,
     error,
@@ -42,10 +47,46 @@ export default function WeaponElementFilter({
           className="flex-col flex-wrap bg-lightbrown border-black border-2 rounded p-2 w-fit"
         >
           <div id="weaponButtons" className="flex justify-center">
+            <div
+              id="weaponButtons-select-unselect"
+              className="flex items-center gap-x-2"
+            >
+              <button
+                className="border-2 border-black rounded p-2 h-1/2 text-black shadow-xl font-montserrat font-bold hover:bg-darkbrown"
+                onClick={() => {
+                  const newMap = new Map(selectedWeaponTypes);
+                  newMap.forEach((value, key) => {
+                    newMap.set(key, true);
+                  });
+                  setSelectedWeaponTypes(newMap);
+                }}
+              >
+                Select All
+              </button>
+              <button
+                className="border-2 border-black rounded p-2 h-1/2 text-black shadow-xl font-montserrat font-bold hover:bg-darkbrown"
+                onClick={() => {
+                  const newMap = new Map(selectedWeaponTypes);
+                  newMap.forEach((value, key) => {
+                    newMap.set(key, false);
+                  });
+                  setSelectedWeaponTypes(newMap);
+                }}
+              >
+                Unselect All
+              </button>
+            </div>
             {TYPES.map((type) => (
               <button
                 key={`weapon-type-${type}`}
-                className="flex border-black border-2 shadow drop-shadow-lg shadow-black bg-darkbrown opacity m-2 p-2 rounded"
+                className={`flex border-black border-2 shadow drop-shadow-lg shadow-black bg-darkbrown m-2 p-2 rounded ${
+                  selectedWeaponTypes.get(type) ? "" : "opacity-50"
+                }`}
+                onClick={() => {
+                  const newMap = new Map(selectedWeaponTypes);
+                  newMap.set(type, !newMap.get(type));
+                  setSelectedWeaponTypes(newMap);
+                }}
               >
                 <Image
                   key={`weapon-type-${type}`}
@@ -81,9 +122,11 @@ export default function WeaponElementFilter({
             </div>
           </div>
           <div id="weaponCards" className="grid grid-cols-3 gap-4">
-            {data.map((weapon) => (
-              <WeaponCard key={weapon.id} weapon={weapon} />
-            ))}
+            {data
+              .filter((weapon) => selectedWeaponTypes.get(weapon.type))
+              .map((weapon) => (
+                <WeaponCard key={weapon.id} weapon={weapon} />
+              ))}
           </div>
         </div>
       )}
